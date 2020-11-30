93b11837159d53a294eb175b35f52435f40dec69
Matheus Xavier Sampaio <xavier_sampaio@alu.ufc.br>
2020-11-30 13:32:14 -0300

    Update 01_install_mk8s.sh

--- deploy-rasa-x/01_install_mk8s.sh
+++ deploy-rasa-x/01_install_mk8s.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install docker.io docker-compose
+sudo apt install docker.io docker-compose -y
 sudo snap install microk8s --classic
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
